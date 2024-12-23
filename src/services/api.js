3b7912cd9280e9f804bd685a9c25e09f8560ea0d
Pedro Mocco <pedromocco00@gmail.com>
2024-12-23 10:21:55 -0400

    cambiar URL de API a HTTP para la función fetchHoroscope

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 export const fetchHoroscope = async (sign) => {
   return await fetch(
-    `https://sandipbgt.com/theastrologer/api/horoscope/${sign}/today/`
+    `http://sandipbgt.com/theastrologer/api/horoscope/${sign}/today/`
   ).then((response) => response.json());
 };
